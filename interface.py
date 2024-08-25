54ce98beb891ba89f73900cd5cbf0f4d335a3e34
Viatok <vit.hanika@gmail.com>
2024-08-25 17:42:55 +0200

    update generatoru

--- interface.py
+++ interface.py
@@ -1,7 +1,10 @@
+import copy
 import random
-import lasersolver.py
+import subprocess
+import sys
 konverzace = True
 def umistuj(objekt, pocet, tabulka):
+    print(tabulka)
     count = 0
     umisteno = 0
     while umisteno < pocet:
@@ -16,7 +19,7 @@ def umistuj(objekt, pocet, tabulka):
             return 0
     return tabulka
 def generate(sirka, vyska, cile, lasery, zdi):
-    tabulka = [[['x']*sirka]*vyska]
+    tabulka = [['x' for i in range(sirka)] for j in range(vyska)]
     tabulka = umistuj('1', cile, tabulka)
     tabulka = umistuj('l', lasery, tabulka)
     tabulka = umistuj('z', zdi, tabulka)
@@ -30,10 +33,15 @@ def generate(sirka, vyska, cile, lasery, zdi):
 while konverzace:
     pozadavek = input('Chcete vstup vygenerovat, nebo zadat souborem? Zadejte g pro generování, s pro soubor.')
     if pozadavek == 'g':
-        parametry = map(int, input('Zadejte jako čísla oddělená mezerami: Šířku, výšku, počet cílů, počet laserů, počet zdí').split())
+        parametry = list(map(int, input('Zadejte jako čísla oddělená mezerami: Šířku, výšku, počet cílů, počet laserů, počet zdí').split()))
         vygenerovane = generate(parametry[0], parametry[1], parametry[2], parametry[3], parametry[4])
         if not type(vygenerovane) == int:
             if __name__ == '__main__':
-                lasersolver.main()
+                with open('generovane.txt', 'w') as f:
+                    f.truncate(0)
+                    for line in vygenerovane:
+                        f.write(' '.join(line) + '\n')
+                    f.close()
+                subprocess.run(['python', 'lasersolver.py'], input=open('generovane.txt', 'r'))
 
 
