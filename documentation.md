b3adf8df023eb926c3536545c8b6979bfc0514ac
viatok <vit.hanika@gmail.com>
2024-08-25 02:48:19 +0200

    komentare

--- documentation.md
+++ documentation.md
@@ -1,2 +1,41 @@
-file
+# Přehled 
+
+Jako zápočtový program jsem vypracoval program řešící jistou logickou úlohu. Umí úlohu vyřešit, a lze s ním generovat zadání těchto úloh i s řešeními. Všechny dokumenty jsou uloženy na githubovém repozitáři. Jeho obsahem jsou tyto soubory:
+
+- lasersolver.py dostane na stdin zadání jedné instance této úlohy, a vypíše do konzole její řešení.
+- interface.py je program, se kterým uživatel primárně komunikuje a spouští ho, tento program umí generovat zadání či číst zadání ze souborů, a zavolá si na ně program solver, čímž tedy nepřímo vypíše řešení.
+- zbylé textové soubory jsou sady vstupů, které se dají spustit programem interface.py
+- a samozřejmě tato dokumentace
+
+
+# Zadání problému
+
+Program řeší tuto úlohu: Mějme tabulku n x m, která má na svých polích tyto objekty:
+
+- Cíle k trefení, reprezentované v textu znakem '1'
+- Zdi, reprezentované znakem 'z'
+- Lasery se čtyřmi orientacemi rovnoběžnými se stěnami tabulky, reprezentovanými znaky '<', '^', '>' a 'v'
+- Prázdná pole, reprezentovaná 'x'
+
+Úlohou je rozmístit na pole zrcadla, reprezentovaná dle jejich orientace buď '\' nebo '/', a to tak, aby po vystřelení všech laserů byl každý cíl zasažen alespoň jednou. Cílem je použít co nejméně zrcadel.
+
+Laser se chová následovně:
+
+- Přes volná pole prochází
+- Cílem prochází
+- Od zdi se odrazí zpátky
+- Od kraje tabulky se odrazí zpátky
+- Od zrcadla se odrazí o 90 stupňů, dle jeho orientace. Pokud však by mělo jít o druhý odraz od téhož zrcadla, místo toho jím proletí.
+
+Časem tedy nutně skončí v nekonečné smyčce, ta je programem po dostatečném čase utnuta.
+
+# Algoritmické řešení
+
+Program lasersolver.py na stdin dostane validní zadání, tedy tabulku celočíselných rozměrů, se znaky oddělenými mezerami.
+
+Zadání si převede do své datové struktury (třída Pole). Na ni volá funkci vyhledej_k, která bude postupně zvyšovat počty zrcadel, vždy zavolá funkci splnitelne, aby zjistila, zda je úloha splnitelná s k zrcadly, načež pokud není, tak zvýší počet zrcadel.
+
+Funkce splnitelne si získá seznam všech možných umístění zrcadel a pro každé takové umístění si vytvoří pomocnou instanci třídy pole, neboli kopii, do které daná zrcadla na daná pole umístí a otestuje, zda je to řešením úlohy (ale dělá to postupně, tedy jméno pomocného plánku zůstává stejné a paměťovou složitost to má O(1)).
+
+Pro otestování, zda je dané rozmístění zrcadel řešením, se spustí funkce vypustlasery, která vystřelí ze všech laserů, načež se zkontroluje, zda byly zasaženy všechny cíle.
 
--- lasersolver.py
+++ lasersolver.py
@@ -5,81 +5,81 @@ import sys
 
 class pole:
     # Základní datová struktura uchovávající zadání problému, její třídy jsou nástroje na jeho řešení
-    def __init__(self, sirka : int, vyska : int, data):
+    def __init__(self, sirka: int, vyska: int, data):
         self.posunybezzrcadla = 0
         self.vyska = vyska
         self.sirka = sirka
         self.data = data
         self.cile = {}
         self.prustrelycilu = {}
-        self.pouzitazrcadla = {(i,j) : 0 for i in range(sirka) for j in range(vyska)}
-        print(self.pouzitazrcadla)
+        self.pouzitazrcadla = {(i, j): 0 for i in range(sirka) for j in range(vyska)}
         self.prazdna_pole = [(i, j) for i in range(sirka) for j in range(vyska) if self.data[i][j] == 'x']
+
     def vytiskni(self):
-        #pomocná funkce, vizuálně vykreslí zadání
+        # pomocná funkce, vizuálně vykreslí zadání
         for line in self.data:
             print(' '.join(map(str, line)))
 
     def v_tabulce(self, x, y):
-        #Vrátí ano, pokud je zadaný bod v tabulce
+        # Vrátí ano, pokud je zadaný bod v tabulce
         if x in range(self.sirka) and y in range(self.vyska):
             return True
         else:
             return False
 
     def volne_pole(self, x, y):
-        #Vrátí ano, pokud je zadaný bod prázdný (není na něm zeď)
+        # Vrátí ano, pokud je zadaný bod prázdný (není na něm zeď)
         volne = ['>', '<', '^', 'v', 'o', 'x', '/', '\\']
         if self.data[x][y] in volne or self.data[x][y].isnumeric():
             return True
         else:
             return False
 
-
     def posouvejlaser(self, x, y, smer):
-        #Simuluje vystřelený laser, rekurzivně se zavolá na to pole a směr, kam se laser posune
+        # Simuluje vystřelený laser, rekurzivně se zavolá na to pole a směr, kam se laser posune
         self.posunybezzrcadla += 1
         if self.posunybezzrcadla > self.vyska * self.sirka:
-            return 1 # Pokud se laser do nekonečna hýbe mezi dvěma zdmi, tímto je zastaven
+            return 1  # Pokud se laser do nekonečna hýbe mezi dvěma zdmi, tímto je zastaven
         aktualni_pole = [x, y]
         cilove_pole = [x + smer[0], y + smer[1]]
-        if self.data[x][y].isnumeric(): #zda jsme zasáhli cíl
+        if self.data[x][y].isnumeric():  # zda jsme zasáhli cíl
             self.prustrelycilu[(x, y)] += 1
-        if self.data[aktualni_pole[0]][aktualni_pole[1]] == '/' and self.pouzitazrcadla[(aktualni_pole[0], aktualni_pole[1])] == 0:
+        if self.data[aktualni_pole[0]][aktualni_pole[1]] == '/' and self.pouzitazrcadla[
+            (aktualni_pole[0], aktualni_pole[1])] == 0:
             self.posunybezzrcadla = 0
             self.pouzitazrcadla[(aktualni_pole[0], aktualni_pole[1])] += 1
             novysmer = [-smer[1], -smer[0]]
             self.posouvejlaser(aktualni_pole[0], aktualni_pole[1], novysmer)
-        elif self.data[aktualni_pole[0]][aktualni_pole[1]] == '\\' and self.pouzitazrcadla[(aktualni_pole[0], aktualni_pole[1])] == 0:
+        elif self.data[aktualni_pole[0]][aktualni_pole[1]] == '\\' and self.pouzitazrcadla[
+            (aktualni_pole[0], aktualni_pole[1])] == 0:
             self.posunybezzrcadla = 0
             self.pouzitazrcadla[(aktualni_pole[0], aktualni_pole[1])] += 1
             novysmer = [smer[1], smer[0]]
-            self.posouvejlaser(aktualni_pole[0] , aktualni_pole[1], novysmer)
+            self.posouvejlaser(aktualni_pole[0], aktualni_pole[1], novysmer)
 
-        elif not self.v_tabulce( cilove_pole[0], cilove_pole[1]):
-            self.posouvejlaser( x,y, [-smer[i] for i in range(2)])
+        elif not self.v_tabulce(cilove_pole[0], cilove_pole[1]):
+            self.posouvejlaser(x, y, [-smer[i] for i in range(2)])
         elif self.volne_pole(cilove_pole[0], cilove_pole[1]):
-            self.posouvejlaser( cilove_pole[0], cilove_pole[1], smer)
+            self.posouvejlaser(cilove_pole[0], cilove_pole[1], smer)
         else:
-            raise Exception('nevalidni pole') #Vždy by měla nastat jedna z předchozích možností.
-
+            raise Exception('nevalidni pole')  # Vždy by měla nastat jedna z předchozích možností.
 
     def vypustlaser(self, x, y):
-        #Vystřelí laser z některého z polí, na kterých se v zadání laser nachází
-        self.pouzitazrcadla = {(i,j) : 0 for i in range(self.sirka) for j in range(self.vyska)}
+        # Vystřelí laser z některého z polí, na kterých se v zadání laser nachází
+        self.pouzitazrcadla = {(i, j): 0 for i in range(self.sirka) for j in range(self.vyska)}
         self.posunybezzrcadla = 0
         if self.data[x][y] == '^':
             self.posouvejlaser(x, y, [-1, 0])
         elif self.data[x][y] == '>':
-            self.posouvejlaser( x, y, [0,1])
+            self.posouvejlaser(x, y, [0, 1])
         elif self.data[x][y] == 'v':
-            self.posouvejlaser( x, y, [1,0])
+            self.posouvejlaser(x, y, [1, 0])
         elif self.data[x][y] == '<':
-            self.posouvejlaser( x, y, [0,-1])
+            self.posouvejlaser(x, y, [0, -1])
         else:
-            raise  Exception('zde nelze vypustit laser')
+            raise Exception('zde nelze vypustit laser')
 
-    def inicializujcile(self): #Nastaví počítadla průstřelů cílů na 0 (předchozí pokusy s nimi mohly pracovat)
+    def inicializujcile(self):  # Nastaví počítadla průstřelů cílů na 0 (předchozí pokusy s nimi mohly pracovat)
         for i in range(self.sirka):
             for j in range(self.vyska):
                 if self.data[i][j].isnumeric():
@@ -93,7 +93,7 @@ class pole:
                     self.vypustlaser(i, j)
 
     def splnitelne(self, n):
-        #Testuje, zda je úloha splnitelná na n zrcadel
+        # Testuje, zda je úloha splnitelná na n zrcadel
         for kombinace in itertools.combinations(self.prazdna_pole, n):
             planeksezrcadly = copy.deepcopy(self)
             planeksezrcadly.data = copy.deepcopy(planeksezrcadly.data)
@@ -107,6 +107,7 @@ class pole:
                 planeksezrcadly.vypustlasery()
 
                 if min(planeksezrcadly.prustrelycilu.values()) > 0:
+                    print('Řešení tohoto plánku je:')
                     planeksezrcadly.vytiskni()
                     return 1
                 planeksezrcadly.data = copy.deepcopy(self.data)
@@ -115,22 +116,23 @@ class pole:
 
 
 
-
-
 zadani = []
-for line in sys.stdin: # Přečte zadání úlohy stdin (tabulku m krát n)
+for line in sys.stdin:  # Přečte zadání úlohy stdin (tabulku m krát n)
     docasna = line.strip().split()
     zadani.append(docasna)
 zadany_objekt = pole(len(zadani), len(zadani[0]), zadani)
-zadany_objekt.vytiskni() #Ukáže zadání pro přehlednost
+zadany_objekt.vytiskni()  # Ukáže zadání pro přehlednost
+
 
 def vsechnyvybery(seznam):
-    #Pomocná funkce, která vrátí všech 2^n kombinací ze seznamu o n prvcích
+    # Pomocná funkce, která vrátí všech 2^n kombinací ze seznamu o n prvcích
     vysledek = []
     for i in range(len(seznam) + 1):
         for kombinace in itertools.combinations(seznam, i):
             vysledek.append(kombinace)
     return vysledek
+
+
 def vyhledej_k(planek):
     # Hlavní funkce, vyhledá minimální počet zrcadel postupným zkoušením
     n = 0
@@ -139,6 +141,8 @@ def vyhledej_k(planek):
         if planek.splnitelne(n):
             return n
     raise Exception('nepovedlo se na 100000000')
+
+
+
+#samotný program
 print(vyhledej_k(zadany_objekt))
-
-
