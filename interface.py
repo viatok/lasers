9f17cc4bee05d2913794da5aad8a90bba4098d00
viatok <vit.hanika@gmail.com>
2024-08-25 02:48:32 +0200

    komentare

---
+++ interface.py
@@ -0,0 +1,39 @@
+import random
+import lasersolver.py
+konverzace = True
+def umistuj(objekt, pocet, tabulka):
+    count = 0
+    umisteno = 0
+    while umisteno < pocet:
+        x = random.randint(0, len(tabulka)-1)
+        y = random.randint(0, len(tabulka[0])-1)
+        if tabulka[x][y] == 'x':
+            tabulka[x][y] = objekt
+            umisteno += 1
+        count += 1
+        if count > (len(tabulka)**2)*len(tabulka[0])**2:
+            print('nepodarilo se vygenerovat')
+            return 0
+    return tabulka
+def generate(sirka, vyska, cile, lasery, zdi):
+    tabulka = [[['x']*sirka]*vyska]
+    tabulka = umistuj('1', cile, tabulka)
+    tabulka = umistuj('l', lasery, tabulka)
+    tabulka = umistuj('z', zdi, tabulka)
+    for x in range(len(tabulka)):
+        for y in range(len(tabulka[0])):
+            if tabulka[x][y] == 'l':
+                orientace = ['^', '>', 'v', '<']
+                zvolenaorientace = random.randint(0, 3)
+                tabulka[x][y] = orientace[zvolenaorientace]
+    return tabulka
+while konverzace:
+    pozadavek = input('Chcete vstup vygenerovat, nebo zadat souborem? Zadejte g pro generování, s pro soubor.')
+    if pozadavek == 'g':
+        parametry = map(int, input('Zadejte jako čísla oddělená mezerami: Šířku, výšku, počet cílů, počet laserů, počet zdí').split())
+        vygenerovane = generate(parametry[0], parametry[1], parametry[2], parametry[3], parametry[4])
+        if not type(vygenerovane) == int:
+            if __name__ == '__main__':
+                lasersolver.main()
+
+
